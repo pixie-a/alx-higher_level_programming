ccd41d9cbfe27952e6f234a8e987b89f02721422
pixie-a <aboukhris33@gmail.com>
2023-11-30 21:04:29 +0100

    3rd fun

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    def arg(argv):
 
-        arg(sys.argv)
-        x = len(argv) - 1
-        if x == 0:
-            print("{}".format(x))
-        else:
-            result = 0
-            i = 1
-            while i <= x:
-                result += int(argv[i])
-                i += 1
-            print("{}".format(result))
+    result = 0
+    for i in range(len(sys.argv) - 1):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
