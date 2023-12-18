9057f6b9bbfbd2376130ecf6763582cad53acde9
pixie-a <aboukhris33@gmail.com>
2023-12-18 21:10:25 +0100

    init fun

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Print x elememts of a list
+   Return: The number of elements printed"""
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        num = 0
+        while num < x:
+            print("{}".format(my_list[num]), end='')
+            num += 1
+        print()
+        return num
+    except IndexError:
+        print()
+        return num
+    pass
