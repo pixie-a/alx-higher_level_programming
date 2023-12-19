8790797ac430f37b62e1cf334d883623f494d5e2
pixie-a <aboukhris33@gmail.com>
2023-12-19 19:22:44 +0100

    1st class sqr

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Class to define a square"""
+
+
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class to define a square by:
+size(private attribute): length of side of the square"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
